6fb078b47a1b28aa2137e114a05989875ba25c01
Glaeesas <JLsniperelite4@outlook.com>
2026-08-22 19:00:35 -0400

    v15 boot 4-part engine loader

--- js/boot-v14.js
+++ js/boot-v14.js
@@ -1,4 +1,4 @@
-// DeadSignal boot v15 - 2-part gzip base64 engine + ui + gore
+// DeadSignal boot v15 - 4-part gzip base64 engine + ui + gore
 (function () {
   function loadScript(src, cb) {
     var s = document.createElement("script");
@@ -7,35 +7,36 @@
     s.onerror = function () { console.error("script fail", src); };
     document.head.appendChild(s);
   }
-  function get(url, cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url + "?v=" + Date.now());
-    xhr.onload = function () {
-      if (xhr.status === 200) cb(xhr.responseText.trim());
-      else console.error("fail", url, xhr.status);
-    };
-    xhr.onerror = function () { console.error("net", url); };
-    xhr.send();
-  }
-  get("js/engine_a.b64", function (a) {
-    get("js/engine_b.b64", function (b) {
-      try {
-        var bin = atob(a + b);
-        var bytes = new Uint8Array(bin.length);
-        for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
-        var stream = new Response(bytes).body.pipeThrough(new DecompressionStream("gzip"));
-        new Response(stream).arrayBuffer().then(function (buf) {
-          var code = new TextDecoder().decode(buf);
-          (0, eval)(code);
-          console.log("[DeadSignal] engine v15 loaded", code.length);
-          loadScript("js/ui.js", function () {
-            loadScript("js/gore.js", function () {
-              console.log("[DeadSignal] ui+gore ready");
-              if (window.__dsBoot) window.__dsBoot();
-            });
-          });
-        }).catch(function (e) { console.error("inflate fail", e); });
-      } catch (e) { console.error("boot fail", e); }
+  function get(url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url + "?v=" + Date.now());
+      xhr.onload = function () {
+        if (xhr.status === 200) resolve(xhr.responseText.trim());
+        else reject(new Error("fail " + url));
+      };
+      xhr.onerror = function () { reject(new Error("net " + url)); };
+      xhr.send();
     });
-  });
+  }
+  Promise.all([0,1,2,3].map(function (i) { return get("js/e" + i + ".b64"); }))
+    .then(function (parts) {
+      var bin = atob(parts.join(""));
+      var bytes = new Uint8Array(bin.length);
+      for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+      return new Response(bytes).body.pipeThrough(new DecompressionStream("gzip"));
+    })
+    .then(function (stream) { return new Response(stream).arrayBuffer(); })
+    .then(function (buf) {
+      var code = new TextDecoder().decode(buf);
+      (0, eval)(code);
+      console.log("[DeadSignal] engine v15 loaded", code.length);
+      loadScript("js/ui.js", function () {
+        loadScript("js/gore.js", function () {
+          console.log("[DeadSignal] ui+gore ready");
+          if (window.__dsBoot) window.__dsBoot();
+        });
+      });
+    })
+    .catch(function (e) { console.error("boot fail", e); });
 })();
