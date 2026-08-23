b9ff024b3ba5b7d67e5143121da577f1ddf8e712
Glaeesas <JLsniperelite4@outlook.com>
2026-08-22 20:43:09 -0400

    v18f boot restore gzip loader

--- js/boot-v14.js
+++ js/boot-v14.js
@@ -1,4 +1,4 @@
-// DeadSignal boot v18f - b64 engine parts + guns
+// DeadSignal boot v18f - gzip engine + guns
 (function () {
   function loadScript(src, cb) {
     var s = document.createElement("script");
@@ -12,24 +12,28 @@
     loadScript(list[i], function () { chain(list, i + 1, done); });
   }
   function loadEngine(cb) {
-    var n = 6, parts = [], loaded = 0;
+    var n = 4, parts = [], loaded = 0;
     function next() {
       if (loaded >= n) {
         try {
-          var js = atob(parts.join(""));
-          (0, eval)(js);
-          console.log("[DeadSignal] engine v18f crouch+guns");
-          cb && cb();
+          var bin = atob(parts.join(""));
+          var bytes = new Uint8Array(bin.length);
+          for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+          var stream = new Response(bytes).body.pipeThrough(new DecompressionStream("gzip"));
+          new Response(stream).arrayBuffer().then(function (buf) {
+            (0, eval)(new TextDecoder().decode(buf));
+            console.log("[DeadSignal] engine v18f crouch+guns");
+            cb && cb();
+          }).catch(function (e) { console.error(e); });
         } catch (e) { console.error(e); }
         return;
       }
       var xhr = new XMLHttpRequest();
-      xhr.open("GET", "js/ep" + loaded + ".b64?v=20260822v18f");
+      xhr.open("GET", "js/ec" + loaded + ".b64?v=20260822v18f");
       xhr.onload = function () {
         if (xhr.status === 200) { parts[loaded] = xhr.responseText.trim(); loaded++; next(); }
         else console.error("chunk", loaded, xhr.status);
       };
-      xhr.onerror = function () { console.error("net ep", loaded); };
       xhr.send();
     }
     next();
